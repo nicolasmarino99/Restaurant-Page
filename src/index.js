0cb6b80813eef6508c97a6e43b09dcbfaf59ef4d
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2020-04-03 17:27:03 -0500

    add webpack config and modules

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+function component() {
+    const element = document.createElement('div');
+  
+   
+    alert('hello world world')
+  
+    return element;
+  }
+  
+  
+  document.body.appendChild(component());
